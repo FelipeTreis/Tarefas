f80d306d2f7ca3ad1efa46dac15749e4ae863eec
Felipe Treis <felipetreis1@gmail.com>
2022-12-28 23:38:40 -0300

    Created Task model and Task model to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_number', 'font_name', 'started', 'finished', 'submeted', )
+    list_display_links = ('task_number', 'font_name', )
+    list_editable = ('started', 'finished', 'submeted', )
+    list_filter = ('task_number', 'font_name', 'started', 'finished', 'submeted', )
+    list_per_page = 10
+    search_fields = ('task_number', 'font_name', )
+    ordering = ('task_number', )
--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    class Meta:
+        verbose_name = 'Tarefa'
+        verbose_name_plural = 'Tarefas'
+
+    start_date = models.DateField('Data inicial', auto_now_add=True)
+    task_number = models.DecimalField('Número tarefa', max_digits=7, decimal_places=4, blank=False, null=False)
+    ssi = models.DecimalField('Número SSI', max_digits=7, decimal_places=4, blank=True, null=True)
+    description = models.TextField('Descrição', blank=False, null=False)
+    font_name = models.CharField('Nome fonte', max_length=50, blank=False, null=False)
+    final_opnion = models.TextField('Parecer Final', blank=True, null=True)
+    started = models.BooleanField('Iniciada', default=False)
+    finished = models.BooleanField('Finalizada', default=False)
+    submeted = models.BooleanField('Submetido', default=False)
+
+    @property
+    def end_date(self):
+        if self.finished == True:
+            self.end_date = date.today()
+
+    def __str__(self):
+        return self.font_name
